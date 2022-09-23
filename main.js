dc7d690d978048fecf917a1bcf54f61811595804
Zin Min Htet <zinminn34119@gmail.com>
2022-09-23 11:43:32 +0630

    Created Scroll Button

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ document.querySelector("#menu-toggle").addEventListener("click",()=>{
     document.querySelector("#nav-menu").classList.toggle("nav-active");
 })
 
+// Scroll Toggle
+
+window.addEventListener("scroll",()=>{
+    if(scrollY > 300){
+        document.querySelector("header").style.backgroundColor ="#9bbe25";
+        document.querySelector(".scroll-top").style.bottom = "30px";
+    }
+    else{
+        document.querySelector("header").style.backgroundColor ="#ffd900";
+        document.querySelector(".scroll-top").style.bottom = "-1000px"
+    }
+})
+
+document.querySelector(".scroll-top").addEventListener("click",()=>{
+    window.scroll(0,0);
+})
+
 // Setting Toggle
 
 document.querySelector("#setting-toggle").addEventListener("click",()=>{
