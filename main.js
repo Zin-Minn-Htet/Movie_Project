37785b64693b1094fb498122a9f8cb7d649603d0
Zin Min Htet <zinminn34119@gmail.com>
2022-09-23 10:58:32 +0630

    Created Setting Function

--- main.js
+++ main.js
@@ -3,3 +3,22 @@
 document.querySelector("#menu-toggle").addEventListener("click",()=>{
     document.querySelector("#nav-menu").classList.toggle("nav-active");
 })
+
+// Setting Toggle
+
+document.querySelector("#setting-toggle").addEventListener("click",()=>{
+    document.querySelector(".setting-box").classList.toggle("setting-box-active");
+});
+
+let images = Array.from(document.querySelectorAll(".setting-box img"));
+
+images.map((image)=>{
+    image.addEventListener("click",()=>{
+        images.forEach((img)=>{
+            img.style.opacity = "1";
+        })
+        document.querySelector(".landing-img").src = image.src;
+        image.style.opacity = "0.5";
+    })
+})
+
