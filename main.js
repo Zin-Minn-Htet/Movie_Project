75fe1616652cf0cb2b4b2d9ee04de6856347ad0a
Zin Min Htet <zinminn34119@gmail.com>
2022-09-24 11:26:48 +0630

    Fetch Movies API

--- main.js
+++ main.js
@@ -39,3 +39,51 @@ images.map((image)=>{
     })
 })
 
+// Fetch Movie
+
+const url = {
+    apiKey : "api_key=a7340b21daf7d49dd052aa6e95cbb512",
+    baseUrl : "https://api.themoviedb.org/3/discover/movie?"
+};
+
+const popularUrl = url.baseUrl+url.apiKey+"&sort_by=popularity.desc";
+const imgUrl = "https://image.tmdb.org/t/p/w500/";
+
+fetchMovie(popularUrl);
+
+function fetchMovie(path){
+    fetch(path)
+    .then(res => res.json())
+    .then(data => showMovie(data))
+};
+
+function showMovie(data){
+    let res = data.results;
+    
+    res.forEach(movie =>{
+        let div = document.createElement("div");
+        div.className = "card";
+        
+        div.innerHTML = `
+        <div class="img-box">
+        <img src="${imgUrl+movie.poster_path}" alt="">
+        </div>
+        <div class="details">
+        <h3>${movie.original_title}</h3>
+        <span>${movie.popularity.toFixed(1)}</span>
+        </div>
+        <div class="overview">
+        <h5>overview</h5>
+        <p>
+        ${movie.overview}
+        </p>
+        <p> 
+        Released at <strong>${movie.release_date}</strong>
+        </p>` ;
+        
+        document.querySelector(".movie-container").appendChild(div);
+        
+    })
+}
+
+
